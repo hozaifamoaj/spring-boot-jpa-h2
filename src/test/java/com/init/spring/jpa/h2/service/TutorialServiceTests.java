c1926f35c0c604a6b69fa099c4b875783eeeae53
hozaifamoaj <hozaifa.moaj@gmail.com>
2024-05-28 16:55:57 +0600

    removed unused imports

--- src/test/java/com/init/spring/jpa/h2/service/TutorialServiceTests.java
+++ src/test/java/com/init/spring/jpa/h2/service/TutorialServiceTests.java
@@ -5,7 +5,6 @@ import com.init.spring.jpa.h2.utils.TemplateData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
